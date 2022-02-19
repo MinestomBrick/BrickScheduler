0bc51953ebb1985d145f1211cad4619be2ac90a6
jorisguffens <joris.guffens@outlook.com>
2022-02-19 19:43:30 +0000

    Deploying to javadocs from @ MinestomBrick/BrickScheduler@853ed846d890ea0239d2ae3b78c8a79f32a5145a 🚀

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.minestombrick.scheduler.api","l":"Scheduler"},{"p":"org.minestombrick.scheduler.api","l":"SchedulerAPI"},{"p":"org.minestombrick.scheduler.api","l":"SchedulerTask"}];updateSearchResults();
